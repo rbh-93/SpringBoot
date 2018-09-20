66f3d295a5c3bc95b0da96f9018ac6c9b8f60931
Rohan <rohanbhattacharya93@gmail.com>
2018-09-20 19:34:38 +0200

    Spring Boot/JPA to create RESTful Services

---
+++ ApiData/src/main/java/rb/topic/TopicService.java
@@ -0,0 +1,58 @@
+package rb.topic;
+
+import java.util.ArrayList;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	@Autowired
+	private TopicRepository topicRepository;
+
+	/*
+	 * private List<Topic> topics = new ArrayList<>(Arrays.asList(
+	 * 
+	 * new Topic("spring", "spring framework", "spring framework description"), new
+	 * Topic("java", "javascript", "javascript description"), new Topic("java",
+	 * "core java", "core java description")));
+	 */
+
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+
+	}
+
+	public Optional<Topic> getTopic(String id) {
+		// return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id);
+	}
+
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		/*
+		 * for (int i = 0; i < topics.size(); i++) { Topic t = topics.get(i); if
+		 * (t.getId().equals(id)) { topics.set(i, topic); return;
+		 * 
+		 * } }
+		 */
+
+		topicRepository.save(topic);
+
+	}
+
+	public void deleteTopic(String id) {
+		// topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.deleteById(id);
+	}
+}
